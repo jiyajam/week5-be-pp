7cdcbb9c71bef787f986b9f3faecb17c9beeb767
Jiya <jiya.kanhirathanpoyil@metropolia.fi>
2025-11-17 15:28:51 +0200

    Complete Iteration-5

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
-const express = require("express");
-const app = express();
-const tourRouter = require("./routes/tourRouter");
-const userRouter = require("./routes/userRouter");
-const { unknownEndpoint } = require("./middleware/customMiddleware");
-const connectDB = require("./config/db"); 
-connectDB();
+const express = require('express')
+const app = express()
+const tourRouter = require('./routes/tourRouter')
+const userRouter = require('./routes/userRouter')
+const {
+  unknownEndpoint,
+  errorHandler,
+} = require('./middleware/customMiddleware')
+const connectDB = require('./config/db')
 
+connectDB()
 
-const morgan = require("morgan");
-app.use(morgan("dev"));
+const morgan = require('morgan')
+app.use(morgan('dev'))
 
 // Middleware to parse JSON
-app.use(express.json());
- 
+app.use(express.json())
+
 // Use the tourRouter for all "/tours" routes
-app.use("/api/tours", tourRouter);
+app.use('/api/tours', tourRouter)
 
 // Use the userRouter for all /users routes
-app.use("/api/users", userRouter);
+app.use('/api/users', userRouter)
+// Example route that throws an error
+app.get('/error', (req, res, next) => {
+  // Trigger an error
+  const error = new Error('Network problem')
+  next(error)
+})
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 // app.use(errorHandler);
+app.use(errorHandler)
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
- 
+  console.log(`Server is running on http://localhost:${port}`)
+})
